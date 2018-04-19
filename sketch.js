289480f0162a2844e41fbfb4ebbaacb17d748dcf
NathanHB <nathan.habib19@gmail.com>
2018-04-19 08:48:23 +0200

    cd
    bdh

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 var character;
 var ground;
 var platforms;
+var plat;
 var wallLeft;
 var jump = 7;
 var gravity = 0.3;
 var jumpAllow = false;
+var bullets;
 
 function setup() {
 	createCanvas(1000, 600);
@@ -21,7 +23,14 @@ function setup() {
 	wallLeft = createSprite(0, height/2, 10, height);
 	wallLeft.immovable = true;
 
+	bullets = new Group();
+
 	platforms = new Group();
+	for (var i = 0; i < 5; i++) {
+		x = Math.floor((Math.random() * 1000) + 1);
+		y = Math.floor((Math.random() * 500) + 1);
+		createPlatform(x, y, 200, 10);
+	}
 
 }
 
@@ -29,11 +38,14 @@ function draw() {
 	background(0);
 	drawSprites();
 
-	jumpAllow = false;
-	if (character.collide(ground)){
-		character.velocity.y = 0;
-		jumpAllow = true;
+	jumpAllow = true;
+	for (var i = 0; i < platforms.length; i++) {
+		if (character.collide(ground) || character.collide(platforms[i])){
+			character.velocity.y = 0;
+			jumpAllow = true;
+		}
 	}
+
 	character.debug = mouseIsPressed;
 	ground.debug = mouseIsPressed;
 	wallLeft.debug = mouseIsPressed;
@@ -49,6 +61,24 @@ function draw() {
 		character.velocity.y = -jump;
 	}
 
+	if (keyWentDown("z")){
+		bullet = createSprite(character.position.x, character.position.y, 10, 10);
+		bullets.add(bullet);
+	}
 
+	for (var i = 0; i < bullets.length; i++){
+		bullets[i].velocity.x = 5;
+		for (var k = 0; k < platforms.length; k++){
+			if(bullets[i].collide(platforms[k])){
+				bullets[i].remove();
+				break
+			}
+		}
+	}
 
+}
+
+function createPlatform(_x, _y, _h, _w){
+	plat = createSprite(_x, _y, _h, _w);
+	platforms.add(plat);
 }
